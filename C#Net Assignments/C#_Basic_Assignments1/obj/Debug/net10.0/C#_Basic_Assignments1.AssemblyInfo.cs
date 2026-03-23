f1c72b911c15df26a86a6050abfc3016e7ed1ed6
pavanmalleswararao <pavanmalleswararao6026@gmail.com>
2026-03-23 21:26:01 +0530

    intial

--- C#Net Assignments/C#_Basic_Assignments1/obj/Debug/net10.0/C#_Basic_Assignments1.AssemblyInfo.cs
+++ C#Net Assignments/C#_Basic_Assignments1/obj/Debug/net10.0/C#_Basic_Assignments1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Basic_Assignments1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e75df42ec89433b817e70451656efb8cf867a20d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096b1f105b7a2a9e3a3e6986c23332d0744aeabb")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Basic_Assignments1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Basic_Assignments1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
