0eeb20fdb2bd6bfb1cdc80cfbc3e1fdb7683ae3f
pavanmalleswararao <pavanmalleswararao6026@gmail.com>
2026-03-24 09:41:22 +0530

    intial

---
+++ C#Net Assignments/C#_MultiThreading/Assignment1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__MultiThreading
+{
+    internal class Assignment1
+    {
+        static int[] partialSums = new int[5];
+
+        static void ProcessNumbers(object obj)
+        {
+            int threadIndex = (int)obj;
+            int start = threadIndex * 10 + 1;
+            int end = start + 9;
+
+            int sum = 0;
+
+            Console.WriteLine($"Thread {threadIndex + 1} processing:");
+
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+                sum += i;
+            }
+
+            Console.WriteLine($"\nThread {threadIndex + 1} Sum: {sum}\n");
+
+            partialSums[threadIndex] = sum;
+        }
+
+        static void Main()
+        {
+            Thread[] threads = new Thread[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                threads[i] = new Thread(ProcessNumbers);
+                threads[i].Start(i);
+            }
+
+            // Wait for all threads
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }
+
+            int finalSum = 0;
+            foreach (int sum in partialSums)
+            {
+                finalSum += sum;
+            }
+
+            Console.WriteLine("Final Sum: " + finalSum);
+        }
+    }
+}
---
+++ C#Net Assignments/C#_MultiThreading/Assignment2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C__MultiThreading_Bank
+{
+    internal class BankAccount
+    {
+        private int Balance = 1000;
+        private readonly object lockObj = new object();
+
+        public void Withdraw(int amount)
+        {
+            lock (lockObj)
+            {
+                if (Balance >= amount)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " is withdrawing " + amount);
+                    Balance -= amount;
+                    Console.WriteLine("Remaining Balance: " + Balance);
+                }
+                else
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " - Insufficient balance!");
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            BankAccount account = new BankAccount();
+
+            Thread t1 = new Thread(() => account.Withdraw(700)) { Name = "User1" };
+            Thread t2 = new Thread(() => account.Withdraw(500)) { Name = "User2" };
+            Thread t3 = new Thread(() => account.Withdraw(300)) { Name = "User3" };
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+    }
+}
